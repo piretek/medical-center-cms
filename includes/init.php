aa0168ce22f3f33edd1ec790decac765074ac9f2
Piotr Czarnecki <piretek@piretek.pro>
2020-05-07 21:40:16 +0200

    Add permission define layer

--- includes/init.php
+++ includes/init.php
@@ -30,12 +30,51 @@ $db = create_database_connection( $config['db'] );
 date_default_timezone_set('Europe/Warsaw');
 
 if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-  $userResult = $db->query(sprintf('SELECT users.id, users.email, users.firstname, users.lastname, roles.name as role FROM users INNER JOIN roles ON roles.id = users.role WHERE users.id = \'%s\'', $db->real_escape_string($_SESSION['user'])));
+  $userResult = $db->query(sprintf('SELECT users.id, users.email, users.firstname, users.lastname, roles.code as roleCode, roles.name as role FROM users INNER JOIN roles ON roles.id = users.role WHERE users.id = \'%s\'', $db->real_escape_string($_SESSION['user'])));
 
   if ($userResult->num_rows != 0) {
     define('AUTHORIZED', true);
 
+    // Get user data
     $authorizedUser = $userResult->fetch_assoc();
+
+    // Fetch all existing roles
+    $userRole = $authorizedUser['roleCode'];
+
+    $roles = $db->query("SELECT * FROM roles")->fetch_all(MYSQLI_ASSOC);
+    $roles = array_map(function($role) {
+      return $role['code'];
+    }, $roles);
+
+    $hasPatientInfo = $db->query(sprintf("SELECT * FROM patients WHERE user = '%d'", $db->real_escape_string($_SESSION['user'])))->num_rows != 0;
+    $hasDoctorInfo = $db->query(sprintf("SELECT * FROM doctors WHERE user = '%d'", $db->real_escape_string($_SESSION['user'])))->num_rows != 0;
+
+    // Define user permissions
+    foreach($roles as $role) {
+
+      if ($role == 'PATIENT' && $hasPatientInfo) {
+        define('IS_'.$role, true);
+      }
+      else {
+        define('IS_'.$role, false);
+      }
+
+      if ($role == 'DOCTOR' && $hasDoctorInfo) {
+        define('IS_'.$role, true);
+      }
+      else {
+        define('IS_'.$role, false);
+      }
+
+      if ($role != 'DOCTOR' && $role != 'PATIENT') {
+        if ($userRole == $role) {
+          define('IS_'.$role, true);
+        }
+        else {
+          define('IS_'.$role, false);
+        }
+      }
+    }
   }
   else {
     define('AUTHORIZED', false);
