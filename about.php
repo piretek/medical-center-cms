ff3371ec9785d30a6182b97d979b879aeffaffbd
psznajder <psznajder127@gmail.com>
2020-05-07 20:37:12 +0200

    Add about page

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php
+
+define('PAGE_TITLE', 'O firmie');
+define('PAGE_NEEDS_AUTHORIZATION', false);
+
+require_once "includes/init.php";
+include_once "views/header.php";
+
+include "views/navs/index-nav.php"; ?>
+
+<div class="info--1">
+  <h2 class="about-header">
+
+  </h2>
+  <p class="about-text">
+
+  </p>
+</div>
+<div class="info--2">
+  <h2 class="about-header">
+
+  </h2>
+  <p class="about-text">
+
+  </p>
+</div>
+<div class="info--3">
+  <h2 class="about-header">
+
+  </h2>
+  <p class="about-text">
+
+  </p>
+</div>
+
+<?php include_once "views/footer.php"; ?>
