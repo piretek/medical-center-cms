31c5b9eb910d3f18377e75895da3eb35371ac9e7
Piotr Czarnecki <piretek@piretek.pro>
2020-05-07 19:57:36 +0200

    Fix auth nav hrefs

--- views/navs/auth-nav.php
+++ views/navs/auth-nav.php
@@ -9,7 +9,7 @@ if (!defined('SECURE_BOOT')) exit();
     <?php if (AUTHORIZED) : ?>
 
     <li>
-      <a href='#'>
+      <a href='user-account.php'>
         <svg class='person-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="18px" height="18px"><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
         <span><?= "{$authorizedUser['firstname']} {$authorizedUser['lastname']}" ?></span>
       </a>
@@ -21,10 +21,10 @@ if (!defined('SECURE_BOOT')) exit();
     <?php else : ?>
 
     <li>
-      <a href='#'>Zaloguj się</a>
+      <a href='auth.php#login'>Zaloguj się</a>
     </li>
     <li>
-      <a href='#'>Zarejestruj się</a>
+      <a href='auth.php#register'>Zarejestruj się</a>
     </li>
 
     <?php endif; ?>
