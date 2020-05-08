444aee3cfaba01df637fa02dc412ac67026e6122
psznajder <psznajder127@gmail.com>
2020-05-08 21:21:23 +0200

    Edit css and about page

--- about.php
+++ about.php
@@ -28,10 +28,10 @@ include "views/navs/index-nav.php"; ?>
         <p>Pacjenci wybierający naszą przychodnię mogą liczyć na profesjonalną pomoc, trafną diagnozę, a co za tym idzie skuteczne leczenie i jak najszybszy powrót do zdrowia. Staramy się, aby nasi pacjenci odczuwali komfort przy każdej wizycie. Wiemy, że nie ma drugiego takiego samego pacjenta i każdy medyczny problem jest niepowtarzalny, dlatego duży nacisk kładziemy na indywidualne podejście.</p>
       </div>
       <div class="about--text">
-        <p>Profesjonalna opieka zdrowotna, jak i wysoki standard udzielanych świadczeń zdrowotnych jest wynikiem umiejętności naszej kadry medycznej. Obecnie świadczymy usługi z zakresu między innymi ortopedii, chirurgii, kardiologii, neurologii, urologii, stomatologii, dermatologii czy ginekologii. Nasza przychodnia to zespół lekarzy z wieloletnim stażem, fenomenalnymi umiejętnościami i bardzo dobrym podejściem do pacjentów. Dzięki temu przychodnia Korona Center jest jednym z wiodących centrów medyczno-diagnostycznych w Lublinie.</p>
+        <p>Profesjonalna opieka zdrowotna, jak i wysoki standard udzielanych świadczeń zdrowotnych jest wynikiem umiejętności naszej kadry medycznej o wieloletnim stażu. Obecnie świadczymy usługi z zakresu między innymi ortopedii, chirurgii, kardiologii, neurologii, urologii, stomatologii, dermatologii czy ginekologii. Dzięki temu jest ona jednym z wiodących centrów medyczno-diagnostycznych w Lublinie.</p>
       </div>
       <div class="about--text">
-        <p>Nasza przychodnia posiada aktualną umowę z Narodowym Funduszem Zdrowia na świadczenie bezpłatnych usług w zakresie podstawowej opieki zdrowotnej.</p>
+        <p>Nasza przychodnia posiada aktualną umowę z Narodowym Funduszem Zdrowia na świadczenie bezpłatnych usług w zakresie podstawowej opieki zdrowotnej. Dzięki temu mogą Państwo skorzystać z bezpłatnych badań diagnostycznych, zabiegów rehabilitacyjnych czy zabiegów szpitalnych. By zarezerwować wizytę refundowaną przez NFZ należy jednak posiadać skierowanie do poradni specjalistycznej. </p>
       </div>
     </div>
   </div>
