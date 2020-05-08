17437444925f87e5104a05633a1d4d18c91a28c8
Michał Kowalski <kowalsky2608@wp.pl>
2020-05-08 23:49:09 +0200

    add register

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
+if (!defined('SECURE_BOOT')) define('SECURE_BOOT', true);
 
-define ('PAGE_TITLE','Logowanie');
-define ('PAGE_NEEDS_AUTHORIZATION', false);
+
 require_once 'includes/init.php';
 
 if (AUTHORIZED) {
@@ -22,14 +22,17 @@ include_once 'views/header.php';
         <?php unset($_SESSION['auth-error']); ?>
       <?php endif; ?>
 
-      <form action = "login.php" method="POST">
+      <form action="login.php" method="POST">
+        <input type="hidden" name="action" value="login">
         <div class="input--container">
           <label class="input--label" for="email"> E-mail: </label>
           <input class="input" id="email" type="text" name="email" placeholder="Podaj e-mail">
+          <span class="input--error"><?php $error = 'login-form-error-email'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div class="input--container">
           <label class="input--label" for="password">Hasło: </label>
           <input class="input" type="password" name="password" id="password" placeholder="Podaj hasło">
+          <span class="input--error"><?php $error = 'login-form-error-password'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div>
           <button class='beautiful' type="submit">Zaloguj</button>
@@ -40,30 +43,36 @@ include_once 'views/header.php';
       <h1>Nie masz konta? Zarejestruj się</h1>
 
       <?php if (isset($_SESSION['auth-error'])) : ?>
-            <span class='error'>Błąd: <?= $_SESSION['auth-error'] ?></span>
-            <?php unset($_SESSION['auth-error']); ?>
+        <span class='error'>Błąd: <?= $_SESSION['auth-error'] ?></span>
+        <?php unset($_SESSION['auth-error']); ?>
       <?php endif; ?>
 
       <form action = "login.php" method="POST">
+        <input type="hidden" name="action" value="register">
         <div class="input--container">
-          <label class="input--label" for="email"> Imię: </label>
+          <label class="input--label" for="name">Imię: </label>
           <input class="input" id="name" type="text" name="name" placeholder="Podaj swoje imię">
+          <span class="input--error"><?php $error = 'register-form-error-name'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div class="input--container">
-          <label class="input--label" for="password">Nazwisko: </label>
-          <input class="input" type="text" name="sname" id="password" placeholder="Podaj swoje nazwisko">
+          <label class="input--label" for="sname">Nazwisko: </label>
+          <input class="input" id='sname' type="text" name="sname" placeholder="Podaj swoje nazwisko">
+          <span class="input--error"><?php $error = 'register-form-error-sname'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div class="input--container">
           <label class="input--label" for="email"> E-mail: </label>
           <input class="input" id="email" type="text" name="email" placeholder="Podaj e-mail">
+          <span class="input--error"><?php $error = 'register-form-error-email'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div class="input--container">
           <label class="input--label" for="password">Hasło: </label>
           <input class="input" type="password" name="password" id="password" placeholder="Podaj hasło">
+          <span class="input--error"><?php $error = 'register-form-error-password'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div class="input--container">
-          <label class="input--label" for="confirm-password">Hasło: </label>
+          <label class="input--label" for="confirm-password">Powtórz hasło: </label>
           <input class="input" type="password" name="confirm-password" id="confirm-password" placeholder="Potwierdź hasło">
+          <span class="input--error"><?php $error = 'register-form-error-confirm-password'; if (isset($_SESSION[$error])) { echo $_SESSION[$error]; unset($_SESSION[$error]); } ?></span>
         </div>
         <div>
           <button class='beautiful' type="submit">Zarejestruj</button>
---
+++ login.php
@@ -0,0 +1,155 @@
+<?php
+
+if (!defined('SECURE_BOOT')) define('SECURE_BOOT', true);
+
+require_once './includes/init.php';
+
+/* - - - - - - - - - - - - - - - - L O G I N - - - - - - - - - - - - - - - - - - - - - */
+
+if (isset($_POST['action']) && $_POST['action'] == 'login') {
+
+  if (isset($_SESSION['user']) && $_SESSION['user'] == 0) {
+    header("Location: {$config['site_url']}/auth.php");
+    exit();
+  }
+
+  $acceptedKeys = [
+    'email',
+    'password'
+  ];
+
+  foreach($acceptedKeys as $key) {
+    if (!array_key_exists($key, $_POST)) {
+      $_SESSION['auth-error'] = 'Niepoprawny login lub hasło.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+    }
+  }
+
+  $email = $db->real_escape_string( htmlentities(strtolower($_POST['email']), ENT_QUOTES, "UTF-8") );
+  $password = $db->real_escape_string( $_POST['password'] );
+
+  $users = $db->query(sprintf("SELECT * FROM users WHERE email = '%s'",
+    $email
+  ));
+
+  if ($users->num_rows == 0) {
+    $_SESSION['auth-error'] = 'Niepoprawny login lub hasło.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+  }
+  else {
+    $user = $users->fetch_assoc();
+
+    if (password_verify($password, $user['password'])) {
+
+      $_SESSION['user'] = (int) $user['id'];
+
+      header("Location: {$config['site_url']}/dashboard.php");
+      exit();
+    }
+    else {
+      $_SESSION['auth-error'] = 'Niepoprawny login lub hasło.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+    }
+  }
+}
+
+/* - - - - - - - - - - - - - - - R E G I S T E R - - - - - - - - - - - - - - - - - - */
+
+else if (isset($_POST['action']) && $_POST['action'] == 'register') {
+
+  if (isset($_SESSION['user']) && $_SESSION['user'] == 0) {
+    header("Location: {$config['site_url']}/auth.php");
+    exit();
+  }
+  
+  $acceptedKeys = [
+    'email',
+    'password',
+    'name',
+    'sname',
+    'confirm-password'
+  ];
+
+  foreach($acceptedKeys as $key) {
+    if (!array_key_exists($key, $_POST)) {
+      $_SESSION['auth-error'] = 'Niepoprawne pola.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+    }
+  }
+
+  $email = htmlentities(strtolower($_POST['email']), ENT_QUOTES, "UTF-8");
+  $password = $_POST['password'] ;
+  $repeatedPassword = $_POST['confirm-password'];
+  $name = htmlentities($_POST ['name'], ENT_QUOTES, "UTF-8");
+  $sname = htmlentities($_POST['sname'], ENT_QUOTES, "UTF-8");
+
+  $ok = true;
+
+  foreach($_POST as $key => $value) {
+    if (empty($_POST[$key])) {
+      $ok = false;
+      $_SESSION["register-form-error-{$key}"] = 'Pole nie może być puste';
+    }
+  }
+  
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $ok = false;
+    $_SESSION["register-form-error-email"] = "Niepoprawny email";
+  }
+
+  if (!preg_match("/^(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/", $password)) {
+    $ok = false;
+    $_SESSION["register-form-error-password"] = "Niepoprawne hasło";
+  }
+
+  if ($password != $repeatedPassword) {
+    $ok = false;
+    $_SESSION["register-form-error-repeat-password"] = "Hasła muszą być takie same";
+  }
+
+  if (strlen($name) > 20){
+    $ok = false;
+    $_SESSION["register-form-error-name"] = "Zbyt duża ilość znaków (maksymalnie 20).";
+  }
+
+  if (strlen($sname) > 25){
+    $ok = false;
+    $_SESSION["register-form-error-sname"] = "Zbyt duża ilość znaków (maksymalnie 25).";
+  }
+
+  if (!$ok) {
+    $_SESSION['auth-error'] = 'Popraw pola';
+    header("Location: {$config['site_url']}/auth.php");
+    exit();
+  }
+  else {
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    $createUserQuery = sprintf("INSERT INTO users (email, password, firstname, lastname, role) VALUES ('%s', '%s','%s','%s', '4')",
+      $db->real_escape_string($email),
+      $db->real_escape_string($hashedPassword),
+      $db->real_escape_string($name),
+      $db->real_escape_string($sname)
+    );
+
+    $response = $db->query($createUserQuery);
+    if ($response) {
+      $_SESSION['auth-success'] = 'Użytkownik pomyślnie zarejestrowany.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+    }
+    else {
+      $_SESSION['auth-error'] = 'Błąd podczas tworzenia użytkownika w bazie danych.';
+      header("Location: {$config['site_url']}/auth.php");
+      exit();
+    }
+  }
+}
+else {
+  header("Location: {$config['site_url']}/auth.php");
+  exit();
+}
