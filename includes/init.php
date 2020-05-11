354d4b041f5c6d72a15d8640362c08469b1f4b6c
Piotr Czarnecki <piretek@piretek.pro>
2020-05-11 20:35:56 +0200

    Add class auto loading. Create user class file

--- includes/init.php
+++ includes/init.php
@@ -3,16 +3,29 @@
 if (!defined('SECURE_BOOT')) exit();
 session_start();
 
-if(!file_exists('./includes/config.php')) {
+// Check if config file exists
+if(!file_exists(__DIR__.'/config.php')) {
   echo "Brak pliku konfiguracyjnego, skopiuj zawartość pliku config.sample.php do pliku config.php utworzonego w folderze includes";
   exit();
 }
 
-$config = require_once './includes/config.php';
+// Load config
+$config = require_once __DIR__.'/config.php';
 
-require_once 'database.php';
-require_once 'functions/verify-config.php';
+// Load database connect functions
+require_once __DIR__.'/database.php';
 
+// Load functions
+foreach ( array_diff( scandir( __DIR__.'/functions' ) , ['.','..']) as $i => $name) {
+  require_once __DIR__.'/functions/'.$name;
+}
+
+// Load classes
+foreach ( array_diff( scandir( __DIR__.'/classes' ) , ['.','..']) as $i => $name) {
+  require_once __DIR__.'/classes/'.$name;
+}
+
+// Check if config file syntax is correct
 $configErrors = verifyConfig($config);
 if (!empty($configErrors)) {
   echo "Błąd w pliku konfiguracyjnym: <br />";
@@ -23,15 +36,21 @@ if (!empty($configErrors)) {
   exit;
 }
 
+// Create database connection
 $db = create_database_connection( $config['db'] );
 
 date_default_timezone_set('Europe/Warsaw');
 
+// Authorize user
 if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
+  // Check if user exists with provided ID in database
   $userResult = $db->query(sprintf('SELECT users.id, users.email, users.firstname, users.lastname, roles.code as roleCode, roles.name as role FROM users INNER JOIN roles ON roles.id = users.role WHERE users.id = \'%s\'', $db->real_escape_string($_SESSION['user'])));
 
   if ($userResult->num_rows != 0) {
+    // Authorize this user, he exists in database
     define('AUTHORIZED', true);
+
+    // Now we need to get user permissions
 
     // Get user data
     $authorizedUser = $userResult->fetch_assoc();
@@ -85,6 +104,7 @@ else {
   define('AUTHORIZED', false);
 }
 
+// Check if page needs user to be logged in. If user is not authorized and page needs it, then redirect him to main url.
 if (defined('PAGE_NEEDS_AUTHORIZATION') && PAGE_NEEDS_AUTHORIZATION && !AUTHORIZED && basename($_SERVER['PHP_SELF']) !== 'index.php') {
   header("Location: {$config['site_url']}/");
   exit;
