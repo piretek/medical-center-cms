8c097ca4872767412e91522f4cb055987adc0699
Piotr Czarnecki <piretek@piretek.pro>
2020-05-12 17:04:54 +0200

    Add create-patient form

--- user-reservations.php
+++ user-reservations.php
@@ -13,6 +13,12 @@ include_once "views/header.php"; ?>
 
   <div class='paper'>
     <h1 class='paper-title'>Historia wizyt</h1>
+
+    <?php if (isset($_SESSION['create-patient-success'])) : ?>
+      <span class='success'><?= $_SESSION['create-patient-success'] ?></span>
+      <?php unset($_SESSION['create-patient-success']); ?>
+    <?php endif; ?>
+
   </div>
 </main>
 
