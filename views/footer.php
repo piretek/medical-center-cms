b5f95eace26375c7c9fb891e323e71a1d5931ed3
Michał Kowalski <kowalsky2608@wp.pl>
2020-05-12 17:19:06 +0200

    add contact to footer add alert to footer

--- views/footer.php
+++ views/footer.php
@@ -6,15 +6,28 @@ if (!defined('SECURE_BOOT')) exit();
 
 
     <div class="footer">
+      <span class="date"></span>
       <div class="footers">
-        <div class="footer-h">
-          Stronę wykonali: 
-          Michal Kowalski | Piotr Czarnecki | Paulina Sznajder
+        <div class="footer-1">
+          <h3>Kontakt</h3>
+          <ul>
+              <li>Telefon: 555-555-555</li>
+              <li>E-mail:korona.center@medical.center</li>
+              <li>Adres: Lublin, ul. Radziwiłowska 13</li>
+            </ul>
         </div>
-        <hr>
-        <div class="footer-c">
-          Copyright © 2020 Korona Center. Wszelkie prawa zastrzeżone.       
-        </div>
+        <div class="footer-2">
+        <img src="./assets/images/warn.png"><h3>COVID-19</h3>
+            <ul>
+              <li>Objawy</li>
+              <li>Profilaktyka</li>
+              <li>Leczenie</li>
+            </ul>
+        </div>  
+      </div>
+      <div class="footer-c">
+        <div>Copyright © 2020 Korona Center. Wszelkie prawa zastrzeżone.</div>      
+        <div>Stronę wykonali: Michal Kowalski | Piotr Czarnecki | Paulina Sznajder</div>
       </div>
     </div>
   </div>
