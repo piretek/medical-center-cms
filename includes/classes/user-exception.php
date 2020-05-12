c54396441499c90a92d4e2d5edb3329bfe425c45
Michał Kowalski <kowalsky2608@wp.pl>
2020-05-12 13:33:11 +0200

    add user exception

---
+++ includes/classes/user-exception.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('SECURE_BOOT')) exit;
+
+class UserException extends Exception  {
+  
+}
+
--- includes/classes/user.php
+++ includes/classes/user.php
@@ -12,68 +12,75 @@ class User {
   public function modify($email, $firstname, $lastname) {
     global $db;
 
-    // Define helping variables
-    $ok = true;
-    $errors = [];
+    try {
+        
+      // Define helping variables
+      $ok = true;
+      $errors = [];
 
-    // Values to check if they are empty
-    $variablesToCheck = [
-      'email' => $email,
-      'firstname' => $firstname,
-      'lastname' => $lastname
-    ];
+      // Values to check if they are empty
+      $variablesToCheck = [
+        'email' => $email,
+        'firstname' => $firstname,
+        'lastname' => $lastname
+      ];
 
-    // Checking these values
-    foreach($variablesToCheck as $key => $value) {
-      if (empty($value)) {
-        $ok = false;
-        $errors[$key] = 'Pole nie może być puste';
-      }
-    }
-
-    // Data sanitization
-    $email = htmlentities(strtolower($email), ENT_QUOTES, "UTF-8");
-    $firstname = htmlentities($firstname, ENT_QUOTES, "UTF-8");
-    $lastname = htmlentities($lastname, ENT_QUOTES, "UTF-8");
-
-    // Get requested user
-    $users = $db->query(sprintf("SELECT * FROM users WHERE email LIKE '%s'",
-      $db->real_escape_string($email)
-    ));
-
-    // Checking if user exists
-    if ($users->num_rows != 0) {
-
-      // If user exists get his data
-      $user = $users->fetch_assoc();
-
-      // Email validation
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !array_key_exists('email', $errors)) {
-        $ok = false;
-        $errors['email'] = "Niepoprawny email";
+      // Checking these values
+      foreach($variablesToCheck as $key => $value) {
+        if (empty($value)) {
+          $ok = false;
+          $errors[$key] = 'Pole nie może być puste';
+        }
       }
 
-      if (!$ok) {
-        // TODO: error handling
-      }
-      else {
-        // Update user's data
-        $successful = $db->query(sprintf("UPDATE users SET email = '%s', firstname = '%s', lastname = '%s' WHERE id = '{$user['id']}'",
-          $db->real_escape_string($email),
-          $db->real_escape_string($firstname),
-          $db->real_escape_string($lastname)
-        ));
+      // Data sanitization
+      $email = htmlentities(strtolower($email), ENT_QUOTES, "UTF-8");
+      $firstname = htmlentities($firstname, ENT_QUOTES, "UTF-8");
+      $lastname = htmlentities($lastname, ENT_QUOTES, "UTF-8");
 
-        // If UPDATE is succesful then overwrite values
-        if ($successful) {
-          $this->email = $email;
-          $this->firstname = $firstname;
-          $this->lastname = $lastname;
+      // Get requested user
+      $users = $db->query(sprintf("SELECT * FROM users WHERE email LIKE '%s'",
+        $db->real_escape_string($email)
+      ));
+
+      // Checking if user exists
+      if ($users->num_rows != 0) {
+
+        // If user exists get his data
+        $user = $users->fetch_assoc();
+
+        // Email validation
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !array_key_exists('email', $errors)) {
+          $ok = false;
+          $errors['email'] = "Niepoprawny email";
+        }
+
+        if (!$ok) {
+          // TODO: error handling
+          throw new UserException();
         }
         else {
-          // TODO: error handling
+          // Update user's data
+          $successful = $db->query(sprintf("UPDATE users SET email = '%s', firstname = '%s', lastname = '%s' WHERE id = '{$user['id']}'",
+            $db->real_escape_string($email),
+            $db->real_escape_string($firstname),
+            $db->real_escape_string($lastname)
+          ));
+
+          // If UPDATE is succesful then overwrite values
+          if ($successful) {
+            $this->email = $email;
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
+          }
+          else {
+            // TODO: error handling
+          }
         }
       }
+    }
+    catch() {
+      
     }
   }
 }
