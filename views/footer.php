f7154fb4a9dbab00702db48cac3545e387b7deea
Michał Kowalski <kowalsky2608@wp.pl>
2020-05-12 16:08:57 +0200

    add footer

--- views/footer.php
+++ views/footer.php
@@ -3,6 +3,20 @@
 if (!defined('SECURE_BOOT')) exit();
 
 ?>
+
+
+    <div class="footer">
+      <div class="footers">
+        <div class="footer-h">
+          Stronę wykonali: 
+          Michal Kowalski | Piotr Czarnecki | Paulina Sznajder
+        </div>
+        <hr>
+        <div class="footer-c">
+          Copyright © 2020 Korona Center. Wszelkie prawa zastrzeżone.       
+        </div>
+      </div>
+    </div>
   </div>
 </body>
 </html>
