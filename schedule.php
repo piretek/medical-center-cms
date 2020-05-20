000fbaf1cafd6dc07ba22b2194bdd2eb73041d65
Piotr Czarnecki <piretek@piretek.pro>
2020-05-21 00:28:10 +0200

    Add working edit schedule form

--- schedule.php
+++ schedule.php
@@ -201,12 +201,11 @@ include_once "views/header.php"; ?>
     <?php notification('error', 'error'); ?>
 
     <?php if (isset($_GET['action']) && !empty($_GET['action'])) :
+      list($d, $m, $y) = explode('-', $_GET['date']);
+      $unixDate = mktime(0,0,0, $m, $d, $y);
 
       switch($_GET['action']) {
         case 'add' :
-
-          list($d, $m, $y) = explode('-', $_GET['date']);
-          $unixDate = mktime(0,0,0, $m, $d, $y);
 
           echo "<h2>Dodawanie nowego wpisu</h2>";
           echo "<p><strong>Lekarz:</strong> {$doctor['degree']} {$doctor['firstname']} {$doctor['lastname']} - {$doctor['specialization']}</p>";
@@ -243,75 +242,95 @@ include_once "views/header.php"; ?>
         case 'edit' :
           echo "<h2>Edycja istniejącego wpisu</h2>";
           echo "<p><strong>Lekarz:</strong> {$doctor['degree']} {$doctor['firstname']} {$doctor['lastname']} - {$doctor['specialization']}</p>";
-          echo "<p><strong>Interwał wizyt:</strong> co ".$seeingInterval." minut</p>";
-          echo "<p><strong>Dzień:</strong> ".str_replace('-','.', $_GET['date'])."</p>";
+          echo "<p><strong>Dzień:</strong> ".weekday(date('w', $unixDate)).', '.str_replace('-','.', $_GET['date'])."</p>";
+          echo "<p><strong>Godziny pracy przychodni:</strong> ".(
+            $mcWorkHours === null
+            ? 'Brak określonych godz. pracy przychodni'
+            : (
+              $mcWorkHours['open-hour'][date('w', $unixDate)] == 0 || $mcWorkHours['close-hour'][date('w', $unixDate)] == 0
+              ? '<span class="error">Nieczynne!</span>'
+              : date('H:i', $mcWorkHours['open-hour'][date('w', $unixDate)]).' - '.date('H:i', $mcWorkHours['close-hour'][date('w', $unixDate)])
+            )
+          )."</p>";
 
           $editForm = new Form('edit');
           $editForm->hidden('type', 'edit');
-          $editForm->text('start', 'Godzina pierwszej wizyty:', '', 'HH:MM');
-          $editForm->text('end', 'Godzina ostatniej wizyty:', '', 'HH:MM');
-          $editForm->text('interval', 'Interwał wizyt:', '', 'co ile minut');
-          $addForm->select('room', 'Gabinet:', $rooms);
+          $editForm->hidden('doctor', $_GET['doctor']);
+          $editForm->hidden('date', $_GET['date']);
+          $editForm->text('start', 'Godzina rozpoczęcia pracy:', date('H:i', $schedule[0]['date']), 'HH:MM');
+          $editForm->text('end', 'Godzina zakończenia pracy:', date('H:i', $schedule[count($schedule) - 1]['date'] + ($schedule[count($schedule) - 1]['date'] - $schedule[count($schedule) - 2]['date'])), 'HH:MM');
+          $editForm->select('interval', 'Interwał wizyt:', [
+            '10' => 'co 10 min',
+            '12' => 'co 12 min',
+            '15' => 'co 15 min',
+            '20' => 'co 20 min',
+            '30' => 'co 30 min',
+            '60' => 'co 60 min',
+          ], ($schedule[count($schedule) - 1]['date'] - $schedule[count($schedule) - 2]['date']));
+          $editForm->select('room', 'Gabinet:', $rooms, $schedule[0]['room']);
           $editForm->place('Edytuj');
           break;
       }
 
     else : ?>
-      <table class='doctor-schedule--table'>
-        <tr>
-          <th class='date'>Dni \ Lekarze</th>
-          <?php
-            $doctorsQuery = sprintf('SELECT doctors.*, users.firstname, users.lastname, specializations.name as specialization FROM (doctors INNER JOIN users ON doctors.user = users.id) INNER JOIN specializations ON doctors.specialization = specializations.id ORDER BY specializations.name, users.lastname ASC');
+      <p>Tabela w przypadku wielu lekarzy, może być przesuwana również w bok</p>
+      <div class="doctor-schedule--table--container">
+        <table class='doctor-schedule--table'>
+          <tr>
+            <th class='date'>Dni \ Lekarze</th>
+            <?php
+              $doctorsQuery = sprintf('SELECT doctors.*, users.firstname, users.lastname, specializations.name as specialization FROM (doctors INNER JOIN users ON doctors.user = users.id) INNER JOIN specializations ON doctors.specialization = specializations.id ORDER BY specializations.name, users.lastname ASC');
 
-            $doctors = $db->query($doctorsQuery);
-            $doctors = $doctors->fetch_all(MYSQLI_ASSOC);
+              $doctors = $db->query($doctorsQuery);
+              $doctors = $doctors->fetch_all(MYSQLI_ASSOC);
+
+              foreach($doctors as $doctor) : ?>
+
+                <th><?= $doctor['degree'].' '.$doctor['firstname'].' '.$doctor['lastname'].'<br />'.$doctor['specialization'] ?></th>
+
+              <?php endforeach;
+            ?>
+          </tr>
+          <?php
+
+          $days = 14;
+
+          for($i = 0; $i < $days; $i++) {
+            echo "<tr>";
+            echo "<td class='date'>".date('d.m.Y', strtotime("today +{$i} days")).'<br>'.weekday(date('w', strtotime("today +{$i} days")))."</td>";
 
             foreach($doctors as $doctor) : ?>
+              <td>
+              <?php
 
-              <th><?= $doctor['degree'].' '.$doctor['firstname'].' '.$doctor['lastname'].'<br />'.$doctor['specialization'] ?></th>
+              $morning = strtotime("today +{$i} days midnight");
+              $evening = strtotime("today +".($i + 1)." days midnight");
 
+              $times = $db->query(sprintf("SELECT schedule.*, rooms.number as room FROM schedule JOIN rooms ON schedule.room = rooms.id WHERE schedule.date BETWEEN '%d' AND '%d' AND schedule.doctor = '%s'",
+                $morning,
+                $evening - 1,
+                $doctor['id']
+              ));
+
+              if ($times->num_rows == 0) { ?>
+                <a class='action-anchor' href='schedule.php?action=add&doctor=<?= $doctor['id'] ?>&date=<?= date('d-m-Y', strtotime("today +{$i} days")) ?>'>Dodaj</a>
+              <?php }
+              else {
+                $times = $times->fetch_all(MYSQLI_ASSOC); ?>
+
+                <a class='action-anchor' href='schedule.php?action=edit&doctor=<?= $doctor['id'] ?>&date=<?= date('d-m-Y', strtotime("today +{$i} days")) ?>'><?= date('H:i', $times[0]['date']).' - '.date('H:i', $times[count($times) - 1]['date'] + ($times[count($times) - 1]['date'] - $times[count($times) - 2]['date'])).' | '.$times[0]['room']; ?></a>
+
+              <?php }
+
+              ?>
+              </td>
             <?php endforeach;
+
+            echo "</tr>";
+          }
           ?>
-        </tr>
-        <?php
-
-        $days = 14;
-
-        for($i = 0; $i < $days; $i++) {
-          echo "<tr>";
-          echo "<td class='date'>".date('d.m.Y', strtotime("today +{$i} days")).'<br>'.weekday(date('w', strtotime("today +{$i} days")))."</td>";
-
-          foreach($doctors as $doctor) : ?>
-            <td>
-            <?php
-
-            $morning = strtotime("today +{$i} days midnight");
-            $evening = strtotime("today +".($i + 1)." days midnight");
-
-            $times = $db->query(sprintf("SELECT schedule.*, rooms.number as room FROM schedule JOIN rooms ON schedule.room = rooms.id WHERE schedule.date BETWEEN '%d' AND '%d' AND schedule.doctor = '%s'",
-              $morning,
-              $evening - 1,
-              $doctor['id']
-            ));
-
-            if ($times->num_rows == 0) { ?>
-              <a class='action-anchor' href='schedule.php?action=add&doctor=<?= $doctor['id'] ?>&date=<?= date('d-m-Y', strtotime("today +{$i} days")) ?>'>Dodaj</a>
-            <?php }
-            else {
-              $times = $times->fetch_all(MYSQLI_ASSOC); ?>
-
-              <a class='action-anchor' href='schedule.php?action=edit&doctor=<?= $doctor['id'] ?>&date=<?= date('d-m-Y', strtotime("today +{$i} days")) ?>'><?= date('H:i', $times[0]['date']).' - '.date('H:i', $times[count($times) - 1]['date'] + ($times[count($times) - 1]['date'] - $times[count($times) - 2]['date'])).' | '.$times[0]['room']; ?></a>
-
-            <?php }
-
-            ?>
-            </td>
-          <?php endforeach;
-
-          echo "</tr>";
-        }
-        ?>
-    </table>
+        </table>
+      </div>
     <?php endif; ?>
   </div>
 </main>
